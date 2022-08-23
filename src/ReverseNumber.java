fe1b779425ab4f820da8bbfeab72c335f6d70319
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-24 01:42:47 +0530

    checking Reverse number

---
+++ src/ReverseNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int a = sc.nextInt();
+        int remainder = 0;
+        int reverse = 0;
+        while (a != 0)  {
+        remainder = a % 10;
+        reverse = reverse * 10 + remainder;
+        a = a /10;
+            }
+        System.out.println(reverse);
+    }
+}
